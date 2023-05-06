7668af7f9ea0a90b7e7d3ee35d4b3619fb6570a0
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2023-05-06 19:49:24 +0530

    Updated About Page

--- frontend/src/pages/guideAndInfoPage.js
+++ frontend/src/pages/guideAndInfoPage.js
@@ -12,11 +12,17 @@ const GuideAndInfoPage = () => {
                 {/*</Head>*/}
                 <div
                     className="px-4 py-10 max-w-3xl mx-auto sm:px-6 sm:py-12 lg:max-w-4xl lg:py-16 lg:px-8 xl:max-w-6xl">
-                    <article class="prose lg:prose-xl">
+                    <article className="prose lg:prose-xl">
                         <h1>What is the purpose of this project 🤔?</h1>
                         <p>
                             The goal of SriOryzaugur is to provide a platform to accurately forecast rice and paddy prices in Sri Lanka 
                         </p>
+                        <br></br>
+                        <h1>What are the different factors that affect rice prices in Sri Lanka 💹?</h1>
+                        <p>
+                            The prices of rice and paddy in Sri Lanka are largely impacted by factors such as the production levels of paddy and rice, the weather conditions that affect the productivity of the crop, demand, and supply of the crop within the country, and the import and export of the crop. In addition to this, the government policies, subsidies, and taxes also play a major role in determining the prices of rice and paddy in Sri Lanka. Furthermore, the demand from other countries and the global policies related to the crop also affect the prices of paddy and rice in Sri Lanka.
+                        </p>
+                        <br></br>
                     </article>
                 </div>
             </div>
