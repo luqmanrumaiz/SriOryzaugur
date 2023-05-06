3d39d701bb4b1153b10b5c0af4f5105a4d0cd216
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2023-05-06 23:26:38 +0530

    Updated TFT and Backend

--- backend/model/temporal_fusion_transformer.py
+++ backend/model/temporal_fusion_transformer.py
@@ -1,6 +1,6 @@
+# Import necessary libraries
 import pandas as pd
 import numpy as np
-import datetime
 from dateutil.relativedelta import relativedelta
 
 import lightning.pytorch as pl
@@ -13,15 +13,25 @@ from pytorch_forecasting.models.temporal_fusion_transformer.tuning import optimi
 
 import torch
 
+# Check if GPU is available
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
+# Define TFT class
 class TFT:
     def __init__(self, data, max_prediction_length=12, batch_size=128):
+        """
+        Initialize TFT class.
+
+        Args:
+            data: pandas DataFrame, input data
+            max_prediction_length: int, maximum number of steps to predict
+            batch_size: int, batch size for training and validation dataloaders
+        """
         self.data = data
         self.max_prediction_length = max_prediction_length
         self.batch_size = batch_size
-        self.static_categoricals = ['series']
+        self.static_categoricals = ['series']  # List of static categorical features
 
         self.training_cutoff = None
         self.max_encoder_length = None
@@ -38,6 +48,13 @@ class TFT:
         self.raw_forecasts = None
 
     def preprocess_data(self, series_to_merge, season_func=None):
+        """
+        Preprocess input data.
+
+        Args:
+            series_to_merge: list of column names to concatenate
+            season_func: function to compute season feature, if any
+        """
         num_of_series = len(series_to_merge)
 
         # concatenate columns based on column names in cols_to_concat
@@ -45,56 +62,96 @@ class TFT:
         combined_series.name = 'y'
         combined_series = pd.DataFrame(combined_series)
 
+        # create 'ds' column
         combined_series['ds'] = pd.concat([self.data['date']] * num_of_series).reset_index(drop=True)
         combined_series['ds'] = pd.to_datetime(combined_series['ds'])
 
+        # create 'series' column
         series_arr = np.arange(num_of_series).reshape(-1, 1)
         combined_series['series'] = np.repeat(series_arr, combined_series.shape[0] / num_of_series)
         combined_series['series'] = combined_series['series'].astype(str)
 
+        # merge with time index
         combined_series = (combined_series.merge(
             (combined_series[['ds']].drop_duplicates(ignore_index=True).rename_axis('time_idx')).reset_index(),
             on=['ds']))
         combined_series['month'] = combined_series['ds'].dt.month
         combined_series['year'] = combined_series['ds'].dt.year
 
+        # add season feature if specified
         if season_func is not None:
             combined_series["season"] = combined_series.apply(season_func, axis=1)
             self.static_categoricals.append('season')
 
+        # update data and set training cutoff
         self.data = combined_series
-
         self.max_encoder_length = self.data.ds.nunique()
         self.training_cutoff = self.data["time_idx"].max() - self.max_prediction_length
 
-    def create_ts_dataset(self, lags=[12]):
+    def create_ts_dataset(self, lags=[12], normalize=True):
+        """
+        Create TimeSeriesDataSet for training and validation.
 
-        self.training = TimeSeriesDataSet(
-            self.data[lambda x: x.time_idx <= self.training_cutoff],
-            time_idx="time_idx",
-            target="y",
-            group_ids=['series'],
-            min_encoder_length=self.max_encoder_length // 2,
-            max_encoder_length=self.max_encoder_length,
-            max_prediction_length=self.max_prediction_length,
-            static_categoricals=self.static_categoricals,
-            time_varying_known_reals=['month', 'year'],
-            time_varying_unknown_reals=['y'],
-            target_normalizer=GroupNormalizer(
-                groups=['series'], transformation='softplus'
-            ),
-            categorical_encoders={'series': NaNLabelEncoder().fit(self.data.series)},
-            lags={'y': lags},
-            add_relative_time_idx=True,
-            add_target_scales=True,
-            add_encoder_length=True,
-        )
+        Args:
+            lags: list of lag values for input features
+            normalize: boolean, whether or not to normalize input data
 
+        Returns:
+            None
+        """
+
+        if normalize:
+            self.training = TimeSeriesDataSet(
+                self.data[lambda x: x.time_idx <= self.training_cutoff],
+                time_idx="time_idx",
+                target="y",
+                group_ids=['series'],
+                min_encoder_length=self.max_encoder_length // 2,
+                max_encoder_length=self.max_encoder_length,
+                max_prediction_length=self.max_prediction_length,
+                static_categoricals=self.static_categoricals,
+                time_varying_known_reals=['month', 'year'],
+                time_varying_unknown_reals=['y'],
+                target_normalizer=GroupNormalizer(
+                    groups=['series'], transformation='softplus'
+                ),
+                categorical_encoders={'series': NaNLabelEncoder().fit(self.data.series)},
+                lags={'y': lags},
+                add_relative_time_idx=True,
+                add_target_scales=True,
+                add_encoder_length=True,
+            )
+        else:
+            self.training = TimeSeriesDataSet(
+                self.data[lambda x: x.time_idx <= self.training_cutoff],
+                time_idx="time_idx",
+                target="y",
+                group_ids=['series'],
+                min_encoder_length=self.max_encoder_length // 2,
+                max_encoder_length=self.max_encoder_length,
+                max_prediction_length=self.max_prediction_length,
+                static_categoricals=self.static_categoricals,
+                time_varying_known_reals=['month', 'year'],
+                time_varying_unknown_reals=['y'],
+                categorical_encoders={'series': NaNLabelEncoder().fit(self.data.series)},
+                lags={'y': lags},
+                add_relative_time_idx=True,
+                add_target_scales=True,
+                add_encoder_length=True,
+            )
+
+        # create validation dataset from training dataset
         self.validation = TimeSeriesDataSet.from_dataset(
             self.training, self.data, predict=True, stop_randomization=True
         )
 
     def create_dataloaders(self):
+        """
+        Create PyTorch DataLoader objects for training and validation datasets.
+
+        Returns:
+            None
+        """
         self.train_dataloader = self.training.to_dataloader(
             train=True, batch_size=self.batch_size, num_workers=0
         )
@@ -103,16 +160,35 @@ class TFT:
         )
 
     def calculate_baseline_error(self):
+        """
+        Calculate baseline error using a simple "last observed value" model.
+
+        Returns:
+            None
+        """
         baseline_predictions = Baseline().predict(self.val_dataloader, return_y=True)
         self.baseline_predictions = baseline_predictions
 
     def optimize_lr(self, plot_lr=False):
+        """
+        Optimize learning rate using PyTorch Lightning's built-in learning rate finder.
+
+        Args:
+            plot_lr: boolean, whether or not to plot the learning rate curve
+
+        Returns:
+            None
+        """
+        # set seed for reproducibility
         pl.seed_everything(42)
+
+        # initialize PyTorch Lightning trainer
         trainer = pl.Trainer(
             accelerator=DEVICE,
             gradient_clip_val=0.1,
         )
 
+        # initialize TFT model with default hyperparameters
         tft = TemporalFusionTransformer.from_dataset(
             self.training,
             learning_rate=0.03,
@@ -124,8 +200,10 @@ class TFT:
             optimizer="Ranger"
         )
 
-        print(f"Number of parameters in network: {tft.size()/1e3:.1f}k")
+        # print number of parameters in the model
+        print(f"Number of parameters in network: {tft.size() / 1e3:.1f}k")
 
+        # run learning rate finder
         res = Tuner(trainer).lr_find(
             tft,
             train_dataloaders=self.train_dataloader,
@@ -134,14 +212,28 @@ class TFT:
             min_lr=1e-6,
         )
 
+        # save suggested learning rate
         self.learning_rate = res.suggestion()
         print(f"suggested learning rate: {res.suggestion()}")
 
+        # plot learning rate curve if requested
         if plot_lr:
             fig = res.plot(show=True, suggest=True)
             fig.show()
 
-    def optimize_hyperparameters(self, n_trials=200, max_epochs=50, use_learning_rate_finder=False):
+    def tune_hyperparameters(self, n_trials=10, max_epochs=50, use_learning_rate_finder=False):
+        """
+        Optimize hyperparameters using Optuna.
+
+        Args:
+            n_trials: int, number of trials to run
+            max_epochs: int, maximum number of epochs per trial
+            use_learning_rate_finder: boolean, whether or not to use PyTorch Lightning's built-in learning rate finder
+
+        Returns:
+            None
+        """
+        # run hyperparameter optimization
         study = optimize_hyperparameters(
             self.train_dataloader,
             self.val_dataloader,
@@ -159,12 +251,30 @@ class TFT:
             use_learning_rate_finder=use_learning_rate_finder,
         )
 
+        # save best hyperparameters
         self.hyperparams = study.best_trial.params
 
     def configure_network_and_trainer(self, hyperparams=None, callbacks=None, max_epochs=30, devices=1,
                                       limit_train_batches=30, enable_model_summary=False, loss=QuantileLoss(),
-                                      log_interval=10, optimizer='Ranger', reduce_on_plateau_patience=4):
+                                      log_interval=10, reduce_on_plateau_patience=4):
+        """
+        Configure TFT model and PyTorch Lightning trainer.
 
+        Args:
+            hyperparams: dictionary of hyperparameters to use (if None, use self.hyperparams)
+            callbacks: list of PyTorch Lightning callbacks
+            max_epochs: int, maximum number of epochs to train for
+            devices: int, number of GPUs to use for training
+            limit_train_batches: int, number of batches to use for each training epoch
+            enable_model_summary: boolean, whether or not to enable PyTorch Lightning's model summary
+            loss: PyTorch loss function to use
+            log_interval: int, how often to log training metrics
+            reduce_on_plateau_patience: int, how many epochs to wait before reducing learning rate on plateau
+
+        Returns:
+            None
+        """
+        # use default hyperparameters if not specified
         if hyperparams is None and self.hyperparams is not None:
             hyperparams = self.hyperparams
         else:
@@ -178,6 +288,7 @@ class TFT:
                 "hidden_continuous_size": 8
             }
 
+        # initialize PyTorch Lightning trainer
         self.trainer = pl.Trainer(
             max_epochs=max_epochs,
             accelerator=DEVICE,
@@ -188,6 +299,7 @@ class TFT:
             callbacks=callbacks,
         )
 
+        # initialize TFT model
         self.tft = TemporalFusionTransformer.from_dataset(
             self.training,
             learning_rate=self.learning_rate if self.learning_rate is not None else hyperparams["learning_rate"],
@@ -197,45 +309,68 @@ class TFT:
             hidden_continuous_size=hyperparams["hidden_continuous_size"],
             loss=loss,
             log_interval=log_interval,
-            optimizer=optimizer,
             reduce_on_plateau_patience=reduce_on_plateau_patience,
         )
 
+        # print number of parameters in model
         print(f"Number of parameters in network: {self.tft.size() / 1e3:.1f}k")
 
     def fit_network(self):
+        """
+        Train TFT model.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        # fit the model
         self.trainer.fit(
             self.tft,
             train_dataloaders=self.train_dataloader,
             val_dataloaders=self.val_dataloader,
         )
 
+        # load best model
         best_model_path = self.trainer.checkpoint_callback.best_model_path
         self.tft = TemporalFusionTransformer.load_from_checkpoint(best_model_path)
 
     def evaluate(self):
+        """
+        Evaluate the trained model.
+
+        Args:
+            None
+
+        Returns:
+            forecasts: list of floats, predicted values for the next 12 months
+            predicted_dates: list of strings, predicted dates for the next 12 months
+            metrics: dictionary of evaluation metrics (MAE, MAPE, SMAPE, RMSE)
+        """
+        # make forecasts
         self.forecasts = self.tft.predict(self.val_dataloader, return_x=True, return_y=True,
                                           trainer_kwargs=dict(accelerator="cpu"))
         self.raw_forecasts = self.tft.predict(self.val_dataloader, mode="raw", return_x=True)
 
+        # generate predicted dates
         date = self.data.iloc[-1]['ds']
         predicted_dates = []
-
         for i in range(self.max_prediction_length):
             date += relativedelta(months=1)
             predicted_dates.append(date)
 
+        # calculate evaluation metrics
         metrics = {
-            'BASELINE MAE': MAE()(self.baseline_predictions.output, self.baseline_predictions.y),
             'MAE': MAE()(self.forecasts.output, self.forecasts.y),
             'MAPE': MAPE()(self.forecasts.output, self.forecasts.y),
             'SMAPE': SMAPE()(self.forecasts.output, self.forecasts.y),
             'RMSE': RMSE()(self.forecasts.output, self.forecasts.y)
         }
-
         for key, val in metrics.items():
             metrics[key] = round(val.item(), 2)
 
+        # format forecasts
         forecasts = self.forecasts.output[0].numpy().tolist()
         forecasts = [float(x) for x in forecasts]
         forecasts = list(map(lambda x: round(x, 2), forecasts))
@@ -243,11 +378,31 @@ class TFT:
         return forecasts, [dt.strftime('%Y-%m-%d') for dt in predicted_dates], metrics
 
     def interpret_model(self):
+        """
+        Plot feature importances of the trained model.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        # calculate feature importances and plot them
         interpretation = self.tft.interpret_output(self.raw_forecasts.output, reduction="sum")
         self.tft.plot_interpretation(interpretation)
 
     def plot_forecasts(self, plot_all_series=False):
+        """
+        Plot the forecasts for a single series or all series.
+
+        Args:
+            plot_all_series: bool, whether to plot forecasts for all series (default=False)
+
+        Returns:
+            None
+        """
         if plot_all_series:
+            # plot forecasts for all series
             for idx in range(self.data.series.nunique()):
                 self.tft.plot_prediction(
                     self.raw_forecasts.x,
@@ -256,4 +411,5 @@ class TFT:
                     add_loss_to_title=True,
                 )
         else:
+            # plot forecasts for a single series
             self.tft.plot_prediction(self.raw_forecasts.x, self.raw_forecasts.output, idx=0)
--- backend/utils.py
+++ backend/utils.py
@@ -1,7 +1,8 @@
+# Import necessary libraries
 import pandas as pd
 import constants
 
-
+# Define a function to classify months as either "maha" or "yala"
 def classify_yaha_mala(row):
     month = row['month']
 
@@ -10,64 +11,89 @@ def classify_yaha_mala(row):
     else:
         return 'yala'
 
-
+# Define a function to retrieve time series data from a MongoDB database
 def retrieve_ts_data(client, selected_series, date_from, date_to):
+    # Generate a list of dates between the specified start and end dates
     dates = pd.date_range(start=date_from, end=date_to, freq='MS')
+    # Create a new DataFrame with the dates as the index
     df = pd.DataFrame({'date': dates})
 
+    # Loop over each selected time series
     for option in selected_series:
         value = option['value']
         collection = value['collection']
         target = value['target']
         col_name = target
         db = client['historical_db']
+        # Check if the collection has associated metadata
         has_metadata = collection in constants.METADATA_FOR_COLLECTION and value.get('metadata') is not None
 
+        # If metadata is present, use it to filter the data from the database
         if has_metadata:
             metadata = value.get('metadata')
             col_name += "_" + metadata[constants.METADATA_FOR_COLLECTION[collection]]
             data = list(db[collection].find(metadata))
+        # If metadata is not present, retrieve all data from the collection
         else:
             data = list(db[collection].find())
 
+        # Convert the retrieved data into a DataFrame
         temp_df = pd.DataFrame(data)
+        # Rename the column containing the time series data to the specified target name
         temp_df.rename(columns={target: col_name}, inplace=True)
+        # Create a list of columns to drop from the DataFrame
         drop_cols = ['_id']
 
+        # If metadata is present or if the collection has associated metadata but no metadata was specified, drop the
+        # metadata column(s)
         if has_metadata or (collection in constants.METADATA_FOR_COLLECTION and value.get('metadata') is None):
             if collection in constants.METADATA_FOR_COLLECTION and value.get('metadata') is None:
                 temp_df = temp_df[temp_df['district'].isnull()]
             drop_cols.append(constants.METADATA_FOR_COLLECTION[collection])
 
+        # Drop the specified columns from the DataFrame
         temp_df.drop(columns=drop_cols, axis=1, inplace=True)
+        # Merge the current DataFrame with the main DataFrame on the date column
         df = pd.merge(df, temp_df, on='date')
 
+    # Return the final DataFrame
     return df
 
-
+# Define a function to generate a summary of a forecast
+# Define a function to generate a summary of a forecast
 def get_forecast_summary(forecasts, forecasted_dates):
-    # Create DataFrame with forecasted values and dates
+    # Create a DataFrame with the forecasted values and dates
     forecast_df = pd.DataFrame({
         'value': forecasts,
         'date': forecasted_dates
     })
 
-    # Compute start and end values for forecast period
+    # Compute the start and end values for the forecast period
     start_value = forecast_df.iloc[0]['value']
     end_value = forecast_df.iloc[-1]['value']
 
-    # Compute overall growth rate for forecast period and annualized growth rate
+    # Compute the overall growth rate for the forecast period and the annualized growth rate
     forecast_growth = (end_value - start_value) / start_value
     annualized_growth = (1 + forecast_growth) ** (12 / len(forecast_df)) - 1
 
-    # Group forecast values by quarter and compute quarterly growth rates
+    # Determine whether the growth is positive or negative and adjust the summary string accordingly
+    if forecast_growth >= 0:
+        summary = f"The forecast predicts a {annualized_growth:.1%} increase over the next 12 months, "
+    else:
+        summary = f"The forecast predicts a {abs(annualized_growth):.1%} decrease over the next 12 months, "
+
+    # Group the forecast values by quarter and compute the quarterly growth rates
     forecast_df['quarter'] = pd.PeriodIndex(forecast_df['date'], freq='Q')
+
+    # Get the sum of the forecast values for each quarter
     quarterly_values = forecast_df.groupby('quarter').sum()
+    # Compute the quarter-to-quarter percentage change in the forecast values and drop missing values
     quarterly_growth = quarterly_values.pct_change().dropna()
 
-    # Find quarter with the highest growth rate and format summary string
+    # Find the quarter with the highest growth rate and format the summary string
     highest_growth_quarter = quarterly_growth.idxmax()[0].strftime('%B')
-    summary = f"The forecast predicts a {annualized_growth:.1%} increase over the next 12 months, " \
-              f"with the largest growth expected in {highest_growth_quarter}."
+    summary += f"with the largest growth expected in {highest_growth_quarter}."
 
+    # Return the summary string
     return summary
+
